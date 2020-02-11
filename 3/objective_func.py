ff119a967efe02c9b9564bae6b217dcc106cd2ad
beachcoder25 <cornish25@gmail.com>
2020-02-11 11:37:24 -0800

    Lab work from 02/11

---
+++ 3/objective_func.py
@@ -0,0 +1,14 @@
+print(cities)
+
+for airport in (airports):
+  (x, y) = airport
+
+  # print(x)
+  # print(y)
+  for city in cities:
+    (xc, yc) = city
+    print(xc)
+    print(yc)
+    
+  # print(xc[0:3])
+  # print(xy[0:3])
