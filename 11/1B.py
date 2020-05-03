dc358fc8c594cf77f3f568e07e2b430bc00d846c
beachcoder25 <cornish25@gmail.com>
2020-05-03 14:18:54 -0700

    1B done

--- 11/1B.py
+++ 11/1B.py
@@ -1,35 +1,60 @@
-def utility_graph(gamma):
+import matplotlib.pyplot as plt
 
-    
+def utility_graph_up(gamma): 
     epsilon = 0
 
     for i in range(99):
-        epsilon = epsilon - 1
+        epsilon = epsilon - (gamma**i)
 
     # print(epsilon)
-    up = (50 * gamma) + epsilon + (10 * gamma)
+    up = (50 * gamma) + epsilon + (10 * (gamma**101))
 
     print(up)
+    return up
 
+
+def utility_graph_down(gamma):
+    epsilon = 0
+
+    for i in range(99):
+        epsilon = epsilon + (gamma**i)
+
+    # print(epsilon)
+    down = (50 * gamma) + epsilon + (10 * (gamma**101))
+
+    print(down)
+    return down
 
 
 def main():
 
     # List of gamma values
-    num = 0.50
-    gamma_list = []
+    num = 0
 
-    for i in range(11):
+    gamma_list = []
+    up_list = []
+    down_list = []
+
+    # Range 21 to go from 0 - 1 in 0.5 increments
+    for i in range(101):
         num = round(num, 2)
+       
         gamma_list.append(num)
         #print(num)
 
-        num = num + 0.05
+        num = num + 0.01
 
     for i in range(len(gamma_list)):
-        print(gamma_list[i])
+        up_list.append(utility_graph_up(gamma_list[i]))
+        down_list.append(utility_graph_down(gamma_list[i]))
 
-    utility_graph(1)
+    # GRAPHING
+
+    plt.plot(gamma_list, up_list, 'r+')
+    plt.plot(gamma_list, down_list, 'b+')
+    
+    plt.axis([0, 1, -1, 70])
+    plt.show()
 
 
 if __name__ == "__main__":
