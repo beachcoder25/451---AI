9f5988811af73d5c08318d9cb3ceee5b978d9988
beachcoder25 <cornish25@gmail.com>
2020-05-04 12:11:51 -0700

    1B completed w/ graph

--- 11/1B.py
+++ 11/1B.py
@@ -1,29 +1,19 @@
 import matplotlib.pyplot as plt
 
 def utility_graph_up(gamma): 
-    epsilon = 0
+    epsilon_up = 0
+    epsilon_down = 0
 
     for i in range(99):
-        epsilon = epsilon - (gamma**i)
+        epsilon_up = epsilon_up - (gamma**i)
+        epsilon_down = epsilon_down + (gamma**i)
 
     # print(epsilon)
-    up = (50 * gamma) + epsilon + (10 * (gamma**101))
+    up = (50 * gamma) + epsilon_up + (10 * (gamma**101))
+    down = (-50 * gamma) + epsilon_down + (-10 * (gamma**101))
 
-    print(up)
-    return up
+    return up, down
 
-
-def utility_graph_down(gamma):
-    epsilon = 0
-
-    for i in range(99):
-        epsilon = epsilon + (gamma**i)
-
-    # print(epsilon)
-    down = (50 * gamma) + epsilon + (10 * (gamma**101))
-
-    print(down)
-    return down
 
 
 def main():
@@ -36,24 +26,29 @@ def main():
     down_list = []
 
     # Range 21 to go from 0 - 1 in 0.5 increments
-    for i in range(101):
+    for i in range(51):
         num = round(num, 2)
        
         gamma_list.append(num)
         #print(num)
 
-        num = num + 0.01
+        num = num + 0.02
 
     for i in range(len(gamma_list)):
-        up_list.append(utility_graph_up(gamma_list[i]))
-        down_list.append(utility_graph_down(gamma_list[i]))
+
+        up, down = utility_graph_up(gamma_list[i])
+        up_list.append(up)
+        down_list.append(down)
 
     # GRAPHING
 
     plt.plot(gamma_list, up_list, 'r+')
     plt.plot(gamma_list, down_list, 'b+')
     
-    plt.axis([0, 1, -1, 70])
+    plt.axis([0, 1, -50, 50])
+    plt.suptitle('Question 1B')
+    plt.xlabel('Gamma Value')
+    plt.ylabel('Utility Value')
     plt.show()
 
 
